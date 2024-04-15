eb520ced807c8c55ac0b91a5953989818b8cded6
mathyasp <mathyastp@gmail.com>
2024-04-15 10:57:13 -0400

    Users can view a list of posts

---
+++ controllers/posts.js
@@ -0,0 +1,30 @@
+const Post = require('../models/post');
+
+module.exports = (app) => {
+  // INDEX
+  app.get('/', async (req, res) => {
+    try {
+      const posts = await Post.find({}).lean();
+      return res.render('posts-index', { posts });
+    } catch (err) {
+      console.log(err.message);
+    }
+  });
+
+  // NEW
+  app.get('/posts/new', (req, res) => {
+    res.render('posts-new');
+  });
+
+  // CREATE
+  app.post('/posts/new', async (req, res) => {
+		try {
+			const post = new Post(req.body);
+			await post.save();
+			res.redirect('/');
+		} catch (error) {
+			console.error('Error saving post:', error);
+			res.status(500).send('Error saving post');
+		}
+	});
+};
--- server.js
+++ server.js
@@ -1,20 +1,15 @@
 const express = require('express');
 const { engine } = require('express-handlebars');
+require('./data/reddit-db');
 
 const app = express();
 
 app.engine('handlebars', engine({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-app.get('/', function(req, res) {
-  res.render('home');
-});
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-// CASES RESOURCE
-
-// NEW
-app.get('/cases/new', (req, res) => {
-  res.render('cases-new', {});
-})
+require('./controllers/posts')(app);
 
 app.listen(3000);
