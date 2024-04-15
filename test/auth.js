f4d63c2f011e3ca6c6a2934351608a7e2230b8f0
mathyasp <mathyastp@gmail.com>
2024-04-15 17:55:29 -0400

    Implemented remaining authentication tests

--- test/auth.js
+++ test/auth.js
@@ -31,6 +31,27 @@ describe('User', function () {
     res.should.have.status(200);
     agent.should.have.cookie('nToken');
   });
+
+  // login
+  it('should be able to login', function (done) {
+    agent
+      .post('/login')
+      .send({ username: 'testone', password: 'password' })
+      .end(function (err, res) {
+        res.should.have.status(200);
+        agent.should.have.cookie('nToken');
+        done();
+      });
+  });
+
+  // logout
+  it('should be able to logout', function (done) {
+    agent.get('/logout').end(function (err, res) {
+      res.should.have.status(200);
+      agent.should.not.have.cookie('nToken');
+      done();
+    });
+  });
 });
 
 after(function () {
